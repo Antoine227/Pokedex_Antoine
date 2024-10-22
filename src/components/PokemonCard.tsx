7ff4cfd81755b5ca5d74268b1ed87595ef20d14a
Antoine227 <antoine_chauvot@hotmail.com>
2024-10-22 16:23:42 +0200

    props2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,21 @@
-function PokemonCard({pokemon}) {
+interface PokemonProps {
+	pokemon: 
+	{
+	name: string;
+	imgSrc?: string;
+	};
+}
+
+function PokemonCard({pokemon}: PokemonProps) {
 	
+
 	return (
 		<figure>
-			 {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}
+			 {pokemon.imgSrc ? 
+			 (<img src={pokemon.imgSrc} 
+				alt={pokemon.name} />) : 
+				(<p>???</p>)
+				}
 			<figcaption>{pokemon.name}</figcaption>
 		</figure>
 	);
